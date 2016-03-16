2ff10078a8d2b1ff4d268db03f8626bed14ef8fc
Albert Fiati-Kumasenu <awkfiati@gmail.com>
2016-03-16 11:54:29 +0000

    Resizing images for ebay picture service modified

--- src/Maverickslab/Ebay/Picture.php
+++ src/Maverickslab/Ebay/Picture.php
@@ -33,31 +33,23 @@
             return null;
         }
 
-        public function uploadToCloudinary($image_path)
-        {
-            Cloudinary::config([
-                "cloud_name" => config('ebay.cloudinary_cloud_name'),
-                "api_key"    => config('ebay.cloudinary_api_key'),
-                "api_secret" => config('ebay.cloudinary_api_secret')
-            ]);
-
-            return Uploader::upload($image_path);
-        }
-
-
         public function resize($image_url)
         {
-            $file_info = pathinfo($image_url);
-
             try {
+                $file_info = pathinfo($image_url);
                 list($original_width, $original_height) = getimagesize(urldecode($image_url));
+
+                if ($original_width >= 1500 || $original_height >= 1500) {
+                    return $image_url;
+                }
+
                 $desired_width = 1500;
-                $desired_height = floor(($original_width * $desired_width) / $original_height);
+                $desired_height = ceil(($desired_width / $original_width) * $original_height);
 
                 $image_url = "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=$image_url&rewriteMime=image/*&resize_w=$desired_width&resize_h=$desired_height&container=url";
 
-                $filename = $file_info['filename'] . "" . uniqid();
-                $filename = storage_path() . "/" . $filename . "." . $file_info['extension'];
+                $filename = "ebay_" . $file_info['filename'] . "" . uniqid();
+                $filename = storage_path() . "/" . $filename . ".jpg";//. $file_info['extension'];
 
                 file_put_contents($filename, file_get_contents($image_url));
 
@@ -73,5 +65,16 @@
                     'message' => 'Image URL not available'
                 ];
             }
+        }
+
+        public function uploadToCloudinary($image_path)
+        {
+            Cloudinary::config([
+                "cloud_name" => config('ebay.cloudinary_cloud_name'),
+                "api_key"    => config('ebay.cloudinary_api_key'),
+                "api_secret" => config('ebay.cloudinary_api_secret')
+            ]);
+
+            return Uploader::upload($image_path);
         }
     }
